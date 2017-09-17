df817a913f081366ea9662d12dd4c0e17bcbf2b0
JanielHNY <janielhny@JanielHNYde-MacBook-Air.local>
2017-09-17 10:34:10 +0930

    user interface and video VC built, segue created.

---
+++ PartyrockMansion/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyrockMansion
+//
+//  Created by JanielHNY on 2017/9/12.
+//  Copyright © 2017年 JanielHNY. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
